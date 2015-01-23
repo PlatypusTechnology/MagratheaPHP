8d54023fced327fdd075bf6eaa7d72d0aa5469e8
paulovelho <paulovelho@paulovelho.com>
2015-01-23 19:44:18 +0000

    removing documentation_bkp

--- documentation_bkp/docs.php
+++
@@ -1,9 +0,0 @@
-<?php include("inc/header.php"); ?>
-
-<?php
-
-	$page = $_GET["p"];
-	if(empty($page)) $page = "index.html";
-	include("docs/".$page);
-
-?>
--- documentation_bkp/index.php
+++
@@ -1,7 +0,0 @@
-<?php include("inc/header.php"); ?>
-
-        Magrathea Framework <br/><br/>
-        Documentation <br/><br/>
-        <em>version: 1.0 beta</em>
-
-<?php include("inc/footer.php"); ?>
