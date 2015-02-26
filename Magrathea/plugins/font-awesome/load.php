f9582a986bfa831e77c4f97e0e547eb881456777
paulovelho <paulovelho@paulovelho.com>
2015-02-26 22:00:51 +0000

    fixing plugins

--- Magrathea/plugins/font-awesome/load.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$pluginFolder = "plugins/Font-awesome";
-
-	MagratheaView::Instance()
-		->IncludeCSS($pluginFolder."/css/font-awesome.min.css")
-		->IncludeCSS($pluginFolder."/css/font-awesome-ie7.min.css");
-
-?>
