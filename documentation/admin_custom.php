cd0a885be711360b3b631af64aad796061ad6cd9
paulovelho <paulovelho@paulovelho.com>
2015-05-22 14:15:11 +0100

    all documentation pages created (still need to fill up content)

---
+++ documentation/admin_custom.php
@@ -0,0 +1,10 @@
+<?php include("inc/header.php"); ?>
+
+<div class="container main_container">
+	<h1>Custom Admin</h1>
+
+
+</div>
+
+
+<?php include("inc/footer.php"); ?>
---
+++ documentation/admin_tests.php
@@ -0,0 +1,10 @@
+<?php include("inc/header.php"); ?>
+
+<div class="container main_container">
+	<h1>Tests</h1>
+
+
+</div>
+
+
+<?php include("inc/footer.php"); ?>
