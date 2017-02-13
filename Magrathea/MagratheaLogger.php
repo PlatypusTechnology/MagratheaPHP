d8747fa38ddd518b544278f7062e75928d53de82
Paulo Henrique Martins <paulovelho@paulovelho.com>
2017-02-13 01:48:00 +0100

    removing one extra line per log

--- Magrathea/MagratheaLogger.php
+++ Magrathea/MagratheaLogger.php
@@ -23,7 +23,7 @@ class MagratheaLogger {
 		$path = MagratheaConfig::Instance()->GetConfigFromDefault("site_path")."/../logs/";
 		if(empty($logFile)) $logFile = "log_".@date("Ym").".txt";
 		$date = @date("Y-m-d h:i:s");
-		$line = "[".$date."] = ".$logThis."\n\n";
+		$line = "[".$date."] = ".$logThis."\n";
 		$file = $path.$logFile;
 		if(!is_writable($path)){
 			$message = "error trying to save file at [".$path."] - confirm permission for writing";
@@ -43,7 +43,7 @@ class MagratheaLogger {
 		if(empty($filename)) $filename = "log_error";
 		$date = @date("Y-m-d_his");
 		$filename .= $date.".txt";
-		$line = "[".$date."] = ".$logThis."\n\n";
+		$line = "[".$date."] = ".$logThis."\n";
 		$file = $path.$filename;
 		if(!is_writable($path)){
 			throw new Exception("error trying to save file at [".$path."] - confirm permission for writing");
