8845f4b3f4f1ef3219fbd9aaaef509c17409fef5
Paulo Velho <paulovelho@paulovelho.com>
2021-01-11 16:47:12 -0300

    getallheaders

--- Magrathea/MagratheaApi.php
+++ Magrathea/MagratheaApi.php
@@ -1,5 +1,17 @@
 <?php
 
+if (!function_exists('getallheaders')){
+	function getallheaders(){
+		$headers = [];
+		foreach ($_SERVER as $name => $value){
+			if (substr($name, 0, 5) == 'HTTP_') {
+				$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+			}
+		}
+		return $headers;
+	}
+}
+
 /**
 * 
 * Creates a server using Magrathea Tools to respond Json files
